8d3b75cfcf79999439606537da8be8f4d09305fd
PietroDC12 <52581321+PietroDC12@users.noreply.github.com>
2021-12-08 14:25:56 -0300

    Revert "ajuste_1"
    This reverts commit ac56ef34b0fa6da0c3ac0e7616b6a07889b29c4c.

--- frontend/src/components/BuscaID.vue
+++ frontend/src/components/BuscaID.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="container">
-        <h1> Busca cachorro</h1>
+    <h1> Busca cachorro</h1>
       <ul>
         <li v-for="info in cadastro" :key="info.cachorro_id"> {{ info.nome_cachorro }}</li>
       </ul>
     <button @click="getFicha">Busca</button>
-    </div>
     </div>
 </template>
  
