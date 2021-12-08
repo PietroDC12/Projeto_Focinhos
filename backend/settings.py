ac56ef34b0fa6da0c3ac0e7616b6a07889b29c4c
PietroDC12 <52581321+PietroDC12@users.noreply.github.com>
2021-12-08 14:15:25 -0300

    ajuste_1

--- backend/settings.py
+++ backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'focinhos-ifc',
         'USER': 'postgres',
-        'PASSWORD': '101010',
+        'PASSWORD': 'pietro12',
         'HOST': 'localhost',
         'PORT': '5432'
     }
@@ -142,4 +143,11 @@ STATIC_URL = '/static/'
 #   ),
 #}
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'djangoProject/static')
+]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
